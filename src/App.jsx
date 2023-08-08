50da88e52abba8e44bd77b7360ee74e718c6487f
Srushti Salvi <srushtisalvi4@gmail.com>
2023-08-08 23:40:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Landing/Navbar";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Coming Soon...</h1>
+    </div>
+  );
+};
+
+export default Landing;
